773b81ec17f3fdf4cd1a7a336314b64799609475
Nolan Splan <nolan.splan@ccisystems.com>
2024-11-07 15:21:14 -0500

    Initial commit

---
+++ src/encryption.rs
@@ -0,0 +1,94 @@
+use aes_gcm::{
+    aead::{Aead, KeyInit},
+    Aes256Gcm, Nonce, Key
+};
+use pbkdf2::pbkdf2_hmac;
+use rand::Rng;
+use sha2::Sha256;
+use serde::{Deserialize, Serialize};
+
+use std::{
+    fs::File,
+    path::PathBuf,
+    io::{BufReader, Read, Write},
+};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct PasswordData {
+    pub passwords: Vec<PasswordEntry>
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct PasswordEntry {
+    pub name: String,
+    pub password: String
+}
+
+pub fn encrypt_data(data: String, master_password: String, filepath: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    let mut salt = [0u8; 16];
+    rand::thread_rng().fill(&mut salt);
+
+    // Derive a 256 bit key from the master password
+    let mut key_bytes = [0u8; 32];
+    pbkdf2_hmac::<Sha256>(master_password.as_bytes(), &salt, 100_000, &mut key_bytes);
+    let key = Key::<Aes256Gcm>::from_slice(&key_bytes);
+
+    let json_bytes = data.as_bytes();
+
+    let cipher = Aes256Gcm::new(key);
+
+    let nonce_array: [u8; 12] = rand::random(); // Create the random nonce as a 12-byte array
+    let nonce = Nonce::from_slice(&nonce_array); // Create nonce from slice
+
+    // Encrypt the JSON data
+    let ciphertext = cipher.encrypt(nonce, json_bytes)
+        .expect("encryption failure!");
+
+    // Save salt, nonce, and ciphertext to a new file
+    let mut encrypted_file = File::create(filepath.clone())?;
+    encrypted_file.write_all(&salt)?;       // Write salt
+    encrypted_file.write_all(nonce)?;       // Write nonce
+    encrypted_file.write_all(&ciphertext)?; // Write ciphertext
+
+    Ok(())
+}
+
+pub fn decrypt_file(filepath: PathBuf, master_password: String) -> Result<PasswordData, Box<dyn std::error::Error>> {
+    // Open the encrypted file and read the contents
+    let mut file = BufReader::new(File::open(filepath)?);
+
+    // Read salt (16 bytes)
+    let mut salt = [0u8; 16];
+    file.read_exact(&mut salt)?;
+
+    // Derive the 256-bit key using PBKDF2-HMAC with the same passphrase and salt
+    let mut key_bytes = [0u8; 32];
+    pbkdf2_hmac::<Sha256>(master_password.as_bytes(), &salt, 100_000, &mut key_bytes);
+    let key = Key::<Aes256Gcm>::from_slice(&key_bytes);
+
+    // Read nonce (12 bytes)
+    let mut nonce_bytes = [0u8; 12];
+    file.read_exact(&mut nonce_bytes)?;
+    let nonce = Nonce::from_slice(&nonce_bytes);
+
+    // Read the rest as the ciphertext
+    let mut ciphertext = Vec::new();
+    file.read_to_end(&mut ciphertext)?;
+
+    // Set up the cipher and decrypt the data
+    let cipher = Aes256Gcm::new(key);
+
+    let decrypted_data = match cipher.decrypt(nonce, ciphertext.as_ref()) {
+        Ok(data) => data,
+        Err(_) => {
+            println!("Decryption failed. Incorrect password or corrupted data.");
+            std::process::exit(1);
+        }
+    };
+
+    // Deserialize decrypted JSON into PasswordData struct
+    let password_data: PasswordData = serde_json::from_slice(&decrypted_data)?;
+
+    Ok(password_data)
+}
+
---
+++ src/main.rs
@@ -0,0 +1,121 @@
+mod encryption;
+
+use home::home_dir;
+use std::{
+    fs, 
+    io, 
+    path::{PathBuf, Path}
+};
+use serde_json::json;
+use crate::encryption::{decrypt_file, encrypt_data, PasswordEntry, PasswordData};
+
+const PASSWORD_FILEPATH: &'static str = ".config/passman/passwords.bin";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let master_password = get_master_password();
+
+    let home_dir = home_dir().expect("Failed to get home directory");
+    let password_file_path = home_dir.join(PASSWORD_FILEPATH);
+
+    if !Path::new(&password_file_path).exists() {
+        set_up_passwords_file(master_password.clone())?;
+    }
+
+    println!("Select an option:");
+    println!("1. Add a new password");
+    println!("2. View all passwords");
+    println!("3. Exit");
+
+    let mut option = String::new();
+
+    io::stdin().read_line(&mut option).expect("Failed to read option");
+
+    match option.trim() {
+        "1" => {
+            add_password(password_file_path.clone(), master_password.clone())?;
+        },
+        "2" => {
+            let password_data = decrypt_file(password_file_path.clone(), master_password.clone())?;
+
+            let passwords = password_data.passwords;
+
+            println!("Passwords:");
+            for password in passwords {
+                println!("     {}: '{}'", password.name, password.password);
+            }
+        },
+        "3" => {
+            println!("Bye!");
+            std::process::exit(0);
+        },
+        _ => {
+            println!("Invalid option");
+        }
+    }
+
+    Ok(())
+}
+
+fn get_master_password() -> String {
+    // Prompt the user for the master password:
+    println!("Please enter your master password:");
+
+    let mut master_password = String::new();
+
+    io::stdin().read_line(&mut master_password).expect("Failed to read master password");
+
+    return master_password;
+}
+
+fn set_up_passwords_file(master_password: String) -> Result<PathBuf, Box<dyn std::error::Error>> {
+    println!("Password file does not exist. Setting up password file...");
+    let home_dir = home_dir().expect("Failed to get home directory");
+
+    let password_file_path = home_dir.join(".config/passman/passwords.bin");
+
+    fs::File::create(&password_file_path).expect("Failed to create password file");
+
+    let initial_data = json!({ "passwords": [] });
+
+    encrypt_data(initial_data.to_string(), master_password.clone(), password_file_path.clone())?;
+
+    return Ok(password_file_path);
+}
+
+fn add_password(filepath: PathBuf, master_password: String) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Please enter the name of the password:");
+
+    let mut name = String::new();
+
+    io::stdin().read_line(&mut name).expect("Failed to read name");
+
+    println!("Please enter the password:");
+
+    let mut password = String::new();
+
+    io::stdin().read_line(&mut password).expect("Failed to read password");
+
+    let password_entry = PasswordEntry {
+        name: name.trim().to_string(),
+        password: password.trim().to_string()
+    };
+
+    let mut decrypted_data: PasswordData = decrypt_file(filepath.clone(), master_password.clone())?;
+
+    // Add the new entry
+    decrypted_data.passwords.push(password_entry.clone());
+
+    // Serialize updated data to JSON
+    let updated_json = serde_json::to_string(&decrypted_data)?;
+
+    println!("Updated JSON: {}", updated_json);
+
+    let home_dir = home_dir().expect("Failed to get home directory");
+    let password_file_path = home_dir.join(PASSWORD_FILEPATH);
+
+    let _ = encrypt_data(updated_json.clone(), master_password.clone(), password_file_path)?;
+
+    println!("Password added."); 
+
+    Ok(())
+}
