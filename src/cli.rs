f61d67207166a4da84c41793b019ad2b9bfe0559
Nolan Splan <nolan.splan@ccisystems.com>
2024-11-11 15:24:29 -0500

    Update help messages for cli args

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,8 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
+#[command(name = "passman")]
+#[command(version, about = "A terminal-based password manager.", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Option<Commands>,
@@ -10,6 +12,7 @@ pub struct Cli {
 #[derive(Subcommand)]
 pub enum Commands {
     // Adds a new password with a name and password
+    #[command(about = "Adds a new password to your encrypted passwords file.", long_about = None)]
     Add {
         #[arg(short, long)]
         name: String,
@@ -17,13 +20,16 @@ pub enum Commands {
         password: String,
     },
     // Gets a password by name
+    #[command(about = "Retrieves the password with the name you've provided.", long_about = None)]
     Get {
         #[arg(short, long)]
         name: String,
     },
     // Lists all passwords
+    #[command(about = "Lists all of the passwords you currently have stored.", long_about = None)]
     List,
     // Removes a password by name
+    #[command(about = "Removes the password with the name you've provided.", long_about = None)]
     Remove {
         #[arg(short, long)]
         name: String,
